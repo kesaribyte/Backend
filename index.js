9b9cf0e5532208cc33f419d333122802e5efb9ff
kesaribyte <shitoleomraje12@gmail.com>
2026-08-31 19:46:51 +0530

    get: gihub add

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/github', (reg, res) => {
+  res.send('kesribyte')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
